ba349006e5e7ddff8c4b453a6d7b3692c94bbe25
Isaac <isaacakalanne@gmail.com>
2020-06-15 11:14:55 +0100

    Created PreviewViewModel.

---
+++ SuperGram/PreviewViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  PreviewViewModel.swift
+//  SuperGram
+//
+//  Created by Isaac Akalanne on 15/06/2020.
+//  Copyright © 2020 Believe And Succeed Apps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class PreviewViewModel {
+    
+    private let preview: Preview
+    
+    public init (preview: Preview) {
+        self.preview = preview
+    }
+    
+    public var image: UIImage {
+        return preview.image.editImage(with: preview.editName)
+    }
+    
+}
+
+extension UIImage {
+    func editImage(with name: String) -> UIImage {
+        return self
+    }
+}
