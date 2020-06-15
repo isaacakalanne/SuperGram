6284fb7e71b60a2f17b3f35543509b08400128f7
Isaac <isaacakalanne@gmail.com>
2020-06-15 13:35:24 +0100

    Created SelfieViewModel file.

---
+++ SuperGram/SelfieViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  SelfieViewModel.swift
+//  SuperGram
+//
+//  Created by Isaac Akalanne on 15/06/2020.
+//  Copyright © 2020 Believe And Succeed Apps. All rights reserved.
+//
+
+import Foundation
+
+public class SelfieViewModel {
+    
+    private let selfie: Selfie
+    
+    public init (selfie: Selfie) {
+        self.selfie = selfie
+    }
+    
+}
