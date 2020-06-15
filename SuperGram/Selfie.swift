2c9f63cd1cb3de11e8fd4e9da28d2d9c4c51ea98
Isaac <isaacakalanne@gmail.com>
2020-06-15 12:08:24 +0100

    Created Selfie class.

---
+++ SuperGram/Selfie.swift
@@ -0,0 +1,20 @@
+//
+//  Selfie.swift
+//  SuperGram
+//
+//  Created by Isaac Akalanne on 15/06/2020.
+//  Copyright © 2020 Believe And Succeed Apps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class Selfie {
+    
+    public let image: UIImage
+    
+    public init(image: UIImage) {
+        self.image = image
+    }
+    
+}
